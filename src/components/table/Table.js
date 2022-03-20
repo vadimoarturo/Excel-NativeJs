d66e5344dffc3b6be6c1ffc5a2df8afa08fbe9be
vburukhin <vburukhin@naumen.ru>
2022-03-20 23:43:07 +0200

    Finish base fraemwork functional

---
+++ src/components/table/Table.js
@@ -0,0 +1,12 @@
+import {ExcelComponent} from '@core/ExcelComponent';
+import {createTable} from './table.template';
+
+export class Table extends ExcelComponent {
+  static className = 'excel__table'
+
+  toHTML() {
+    return createTable()
+  }
+}
+
+
--- src/index.js
+++ src/index.js
@@ -1,2 +1,13 @@
+import {Excel} from '@/components/excel/Excel'
+import {Header} from '@/components/header/Header'
+import {Formula} from '@/components/formula/Formula'
+import {Table} from '@/components/table/Table'
+import {Toolbar} from '@/components/toolbar/Toolbar'
 import './scss/index.scss'
-console.log('Hello world!');
+
+const excel = new Excel('#app', {
+  components: [Header, Toolbar, Formula, Table],
+})
+
+excel.render()
+
